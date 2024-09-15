0537c9e7adc4e87f88df7ae5b74da489d049da76
oumaimakhlifi <oumaima.khlifi@esprit.tn>
2024-09-16 00:14:05 +0100

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div className='flex flex-col items-center justify-center min-h-screen'>
+        <h1 className='text-center sm:text-4xl text-2xl font-bold my-6 text-gray-900'>Hello, welcome to About !!</h1>
+        <p className='text-center leading-relaxed text-base mb-4'>
+          Nous sommes ravis de vous accueillir sur notre site web. Ici, vous trouverez des informations intéressantes, des articles informatifs, et des ressources utiles pour vous aider dans vos projets. Notre objectif est de vous fournir le meilleur contenu et de vous offrir une expérience utilisateur agréable. N'hésitez pas à explorer les différentes sections et à nous contacter si vous avez des questions ou des commentaires.
+        </p>
+        <p className='text-center leading-relaxed text-base mb-4'>
+          Nous sommes ravis de vous accueillir sur notre site web. Ici, vous trouverez des informations intéressantes, des articles informatifs, et des ressources utiles pour vous aider dans vos projets. Notre objectif est de vous fournir le meilleur contenu et de vous offrir une expérience utilisateur agréable. N'hésitez pas à explorer les différentes sections et à nous contacter si vous avez des questions ou des commentaires.
+        </p>
+        <p className='text-center leading-relaxed text-base mb-4'>
+          Nous sommes ravis de vous accueillir sur notre site web. Ici, vous trouverez des informations intéressantes, des articles informatifs, et des ressources utiles pour vous aider dans vos projets. Notre objectif est de vous fournir le meilleur contenu et de vous offrir une expérience utilisateur agréable. N'hésitez pas à explorer les différentes sections et à nous contacter si vous avez des questions ou des commentaires.
+        </p>
+    </div>
+  );
+};
+
+export default About;
